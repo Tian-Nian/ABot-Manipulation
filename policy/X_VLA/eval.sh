10d1f8389ff3eca95ce5d99503e55067b134d3bd
Tian-Nian <1250576969@qq.com>
2026-05-16 15:08:11 +0800

    update xvla eval.sh .

--- policy/X_VLA/eval.sh
+++ policy/X_VLA/eval.sh
@@ -4,18 +4,21 @@ set -e
 policy_name=X_VLA
 dataset_name=${1}
 task_name=${2}
-env_cfg=${3}
-expert_data_num=${4}
-action_type=${5:-ee}
-gpu_id=${6}
+ckpt_name=${3}
+env_cfg_type=${4}
+expert_data_num=${5}
+action_type=${6}
 seed=${7}
-policy_conda_env=${8}
-eval_env_conda_env=${9}
+policy_gpu_id=${8}
+env_gpu_id=${9}
+policy_conda_env=${10}
+eval_env_conda_env=${11}
+
 MODEL_PATH=${10}
 PROCESSOR_PATH=${11}
 
-export CUDA_VISIBLE_DEVICES="${gpu_id}"
-echo -e "\033[33m[INFO] GPU ID (to use): ${gpu_id}\033[0m"
+export CUDA_VISIBLE_DEVICES="${policy_gpu_id}"
+echo -e "\033[33m[INFO] GPU ID (to use): ${policy_gpu_id}\033[0m"
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../.." && pwd)"
 UTILS_DIR="${ROOT_DIR}/XPolicyLab/utils"
@@ -37,6 +40,7 @@ python "${ROOT_DIR}/XPolicyLab/setup_policy_server.py" \
     --config_path "${yaml_file}" \
     --overrides \
         port="${FREE_PORT}" \
+        dataset_name="${dataset_name}" \
         task_name="${task_name}" \
         env_cfg="${env_cfg}" \
         expert_data_num="${expert_data_num}" \
@@ -51,5 +55,6 @@ SERVER_PID=$!
 echo -e "\033[32m[SERVER] PID=${SERVER_PID} (running in background)\033[0m"
 
 # ==================== 启动 client 进行评测 ====================
-bash "${UTILS_DIR}/setup_env_client.sh" "${UTILS_DIR}" "${yaml_file}" "${eval_env_conda_env}" "${FREE_PORT}" "${dataset_name}" "${task_name}" "${env_cfg_type}" "${policy_name}" "${ROOT_DIR}"
+additional_info="ckpt_name=${ckpt_name},action_type=${action_type}"
+bash "${UTILS_DIR}/setup_env_client.sh" "${UTILS_DIR}" "${yaml_file}" "${eval_env_conda_env}" "${FREE_PORT}" "${dataset_name}" "${task_name}" "${env_cfg_type}" "${policy_name}" "${additional_info}" "${ROOT_DIR}" "${seed}" "${env_gpu_id}"
 echo -e "\033[33m[MAIN] eval_policy_client has finished; cleaning up server.\033[0m"
